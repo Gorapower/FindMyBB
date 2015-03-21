986b43b917d1878f8c7c6dd76df007838487965f
Gorapower <Alexis@Alfredos-MacBook-Pro.local>
2015-03-21 00:13:22 -0600

    inicial commit

---
+++ FindMyBBTests/FindMyBBTests.swift
@@ -0,0 +1,36 @@
+//
+//  FindMyBBTests.swift
+//  FindMyBBTests
+//
+//  Created by Alexis on 3/21/15.
+//  Copyright (c) 2015 Alfredo Alexis Gonzalez Rodriguez. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FindMyBBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
